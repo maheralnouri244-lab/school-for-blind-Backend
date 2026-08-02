d25c36b471e6906545bf09d6089fec46d7af6ebd
Ayahammamieh <ayahammamieh88@gmail.com>
2026-08-02 10:20:13 +0300

    seederschedule

--- school_for_blind/database/seeders/scheduleSeeder.php
+++ school_for_blind/database/seeders/scheduleSeeder.php
@@ -10,14 +10,14 @@ class scheduleSeeder extends Seeder
     public function run()
     {
         $schedules = [
-            ['class_id' => 1, 'teacher_id' => 1, 'subject_id' => '1', 'day_of_week' =>'الأحد', 'period_number' => 1, 'start_time' => '08:00:00', 'end_time' => '08:45:00'],
-            ['class_id' => 1, 'teacher_id' => 1, 'subject_id' => '1', 'day_of_week' =>'الاثنين', 'period_number' => 2, 'start_time' => '09:00:00', 'end_time' => '09:45:00'],
-            ['class_id' => 1, 'teacher_id' => 2, 'subject_id' => '2',   'day_of_week' =>'الثلاثاء', 'period_number' => 3, 'start_time' => '10:00:00', 'end_time' => '10:45:00'],
-            ['class_id' => 1, 'teacher_id' => 3, 'subject_id' => '5',  'day_of_week' =>'الأربعاء', 'period_number' => 4, 'start_time' => '11:00:00', 'end_time' => '11:45:00'],
+            ['class_id' => 1, 'teacher_id' => 1, 'subject_id' => '1', 'day_of_week' =>'1', 'period_number' => 1, 'start_time' => '08:00:00', 'end_time' => '08:45:00'],
+            ['class_id' => 1, 'teacher_id' => 1, 'subject_id' => '1', 'day_of_week' =>'2', 'period_number' => 2, 'start_time' => '09:00:00', 'end_time' => '09:45:00'],
+            ['class_id' => 1, 'teacher_id' => 2, 'subject_id' => '2',   'day_of_week' =>'3', 'period_number' => 3, 'start_time' => '10:00:00', 'end_time' => '10:45:00'],
+            ['class_id' => 1, 'teacher_id' => 3, 'subject_id' => '5',  'day_of_week' =>'4', 'period_number' => 4, 'start_time' => '11:00:00', 'end_time' => '11:45:00'],
             
-            ['class_id' => 1, 'teacher_id' => 4, 'subject_id' => '10','day_of_week' => 'الخميس', 'period_number' => 1, 'start_time' => '08:00:00', 'end_time' => '08:45:00'],
-            ['class_id' => 1, 'teacher_id' => 4, 'subject_id' => '4','day_of_week' =>'الأحد', 'period_number' => 2, 'start_time' => '09:00:00', 'end_time' => '09:45:00'],
-            ['class_id' => 1, 'teacher_id' => 2, 'subject_id' => '3',   'day_of_week' =>'الاثنين', 'period_number' => 3, 'start_time' => '10:00:00', 'end_time' => '10:45:00'],
+            ['class_id' => 1, 'teacher_id' => 4, 'subject_id' => '10','day_of_week' => '5', 'period_number' => 1, 'start_time' => '08:00:00', 'end_time' => '08:45:00'],
+            ['class_id' => 1, 'teacher_id' => 4, 'subject_id' => '4','day_of_week' =>'1', 'period_number' => 2, 'start_time' => '09:00:00', 'end_time' => '09:45:00'],
+            ['class_id' => 1, 'teacher_id' => 2, 'subject_id' => '3',   'day_of_week' =>'2', 'period_number' => 3, 'start_time' => '10:00:00', 'end_time' => '10:45:00'],
         ];
 
         foreach ($schedules as &$schedule) {
