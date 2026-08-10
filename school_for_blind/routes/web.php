efcd6e132fc7b00af562e57ea8ade65152da6e05
maheralnouri244-lab <maheralnouri244@gmail.com>
2026-08-10 23:02:11 +0300

    .

--- school_for_blind/routes/web.php
+++ school_for_blind/routes/web.php
@@ -191,11 +191,10 @@ Route::middleware([CheckAdminRole::class . ':Super Admin,Academic Manager,Modera
     Route::get('/students/{student}/reports', [StudentReportWebController::class, 'index'])->name('students.reports');
 });
 
-<<<<<<< HEAD
 Route::get('/fcm-test', function () {
     return view('fcm_test');
 });
-=======
+
 Route::middleware([CheckAdminRole::class . ':Super Admin,Academic Manager,Moderator'])->group(function () {
     Route::get('/announcements', function () {
         return view('pages.announcements.index');
@@ -203,4 +202,3 @@ Route::middleware([CheckAdminRole::class . ':Super Admin,Academic Manager,Modera
     Route::get('/api/dashboard/announcements', [AnnouncementDashboardController::class, 'index'])->name('dashboard.announcements.api');
     Route::post('/api/dashboard/announcements', [AnnouncementDashboardController::class, 'store'])->name('dashboard.announcements.store');
 });
->>>>>>> 99372a55b3e58d89b81d7b26ec28eb4e90479f86
