78b83b9ddd8304a7085b5d3eaad81ff4e26f2423
Ayahammamieh <ayahammamieh88@gmail.com>
2026-08-21 16:33:24 +0300

    .

--- school_for_blind/database/migrations/2026_04_26_045045_create_students_table.php
+++ school_for_blind/database/migrations/2026_04_26_045045_create_students_table.php
@@ -48,7 +48,6 @@ return new class extends Migration {
             $table->boolean('notifications_enabled')->default(true);
            $table->softDeletes();
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
